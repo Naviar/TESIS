048b9d4b226e8d44fd3140a07ee22f30b9a69d9c
Iván David Arango Lizarazo <ing.ivandarango@gmail.com>
2019-10-26 00:22:27 -0500

    arreglando conexion a bd

--- Fuentes/Back/controller/subirarchivos.controller.js
+++ Fuentes/Back/controller/subirarchivos.controller.js
@@ -1,6 +1,7 @@
 const subirarchivosCtrl = {};
 var ibmdb = require("ibm_db");
 let connStr = require('../database');
+let db = require('../db_connection');
 const nodemailer = require('nodemailer');
 //autenticacion para enviar correo
 var transporter = nodemailer.createTransport({
@@ -11,45 +12,41 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+
 subirarchivosCtrl.upload = (req, res) => {
 
-    console.log(req.info);
 
-    ibmdb.open(connStr, (err, conn) => {
 
-        conn.query("SELECT * FROM usuario", (err, data) => {
-            if (err) {
-                res.json({ error: err })
-                console.log("Hubo un error en la busqueda" + err);
-            } else {
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos")
-                })
-                res.json({ data: data })
-            }
-        })
-    })
+
+
+    db.query("SELECT * FROM usuario", (err, data) => {
+        if (err) {
+            res.json({ error: err })
+            console.log("Hubo un error en la busqueda" + err);
+        } else {
+
+            res.json({ data: data })
+        }
+    });
+
 }
 
 subirarchivosCtrl.getAnnouncementCurrent = (req, res) => {
 
     try {
         query = `select * from convocatorias where id_convocatoria = (select max(id_convocatoria) from convocatorias)`;
-        ibmdb.open(connStr, (err, conn) => {
 
-            conn.query(query, (err, data) => {
-                if (err) {
-                    res.json({ error: err })
-                    console.log("Hubo un error en la busqueda" + err);
-                } else {
-                    conn.close(() => {
-                        console.log("Se ha cerrado la base de datos")
-                    })
-                    console.log(`encontro esta announcement ${JSON.stringify(data[0])}`);
-                    res.json(data[0]);
-                }
-            })
-        });
+
+        db.query(query, (err, data) => {
+            if (err) {
+                res.json({ error: err })
+                console.log("Hubo un error en la busqueda" + err);
+            } else {
+                console.log(`encontro esta announcement ${JSON.stringify(data[0])}`);
+                res.json(data[0]);
+            }
+        })
+
     } catch (error) {
 
         res.state(500).json({ error });
@@ -60,40 +57,36 @@ subirarchivosCtrl.getAnnouncementCurrent = (req, res) => {
 
 subirarchivosCtrl.getDocumentos = (req, res) => {
 
-    ibmdb.open(connStr, (err, conn) => {
 
-        conn.query("SELECT * FROM documento", (err, data) => {
-            if (err) {
-                res.json({ error: err })
-                console.log("Hubo un error en la busqueda" + err);
-            } else {
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos")
-                })
-                res.json(data);
-            }
-        })
+
+    db.query("SELECT * FROM documento", (err, data) => {
+        if (err) {
+            res.json({ error: err })
+            console.log("Hubo un error en la busqueda" + err);
+        } else {
+
+            res.json(data);
+        }
     });
+
 }
 
 subirarchivosCtrl.getProyectosByEtapa = (req, res) => {
 
     const etapa = req.params.etapa;
 
-    ibmdb.open(connStr, (err, conn) => {
 
-        conn.query(`SELECT p.* , u.NOMBRE, u.APELLIDO, u.CORREO , U.celular FROM proyecto as p INNER JOIN usuario as u ON p.USUARIO_ID_USUARIO = u.ID_USUARIO where p.ETAPA = '${etapa}' AND p.ID_CONVOCATORIA_ID = (SELECT MAX(ID_CONVOCATORIA) FROM CONVOCATORIAS)`, (err, data) => {
-            if (err) {
-                res.json({ error: err })
-                console.log("Hubo un error en la busqueda" + JSON.stringify(err));
-            } else {
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos")
-                })
-                res.json(data);
-            }
-        })
-    })
+
+    conn.query(`SELECT p.* , u.NOMBRE, u.APELLIDO, u.CORREO , U.celular FROM proyecto as p INNER JOIN usuario as u ON p.USUARIO_ID_USUARIO = u.ID_USUARIO where p.ETAPA = '${etapa}' AND p.ID_CONVOCATORIA_ID = (SELECT MAX(ID_CONVOCATORIA) FROM CONVOCATORIAS)`, (err, data) => {
+        if (err) {
+            res.json({ error: err })
+            console.log("Hubo un error en la busqueda" + JSON.stringify(err));
+        } else {
+
+            res.json(data);
+        }
+    });
+
 }
 subirarchivosCtrl.updateFixes = (req, res) => {
 
@@ -105,34 +98,32 @@ subirarchivosCtrl.updateFixes = (req, res) => {
         const documento = req.body.documento;
 
 
-        ibmdb.open(connStr, (err, conn) => {
 
-            conn.query(`UPDATE PROYECTO SET CORRECCIONES = '${state}', CORREGIDO = '${!state}'   WHERE ID_PROYECTO = '${ID_PROYECTO}' `, (err, data) => {
 
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos")
-                });
-                const mailOptions = {
-                    from: 'consultorio.usta.DRSU@gmail.com', // dirección del remitente 
-                    to: `${correo}`, // lista de los destinatarios del 
-                    subject: `tiene correciones para el proyecto ${nombreProyecto}`, // Línea del asunto 
-                    html: `<h1>CORRECCIONES PENDIENTES</h1>
+        db.query(`UPDATE PROYECTO SET CORRECCIONES = '${state}', CORREGIDO = '${!state}'   WHERE ID_PROYECTO = '${ID_PROYECTO}' `, (err, data) => {
+
+
+            const mailOptions = {
+                from: 'consultorio.usta.DRSU@gmail.com', // dirección del remitente 
+                to: `${correo}`, // lista de los destinatarios del 
+                subject: `tiene correciones para el proyecto ${nombreProyecto}`, // Línea del asunto 
+                html: `<h1>CORRECCIONES PENDIENTES</h1>
                         <p>tu proyecto <b>${nombreProyecto}</b> tiene correciones pendientes para el documento ${documento}</p>
                         <p>por favor entrar a la plataforma y subir el documento atendiendo las correciones , gracias.</p>` // cuerpo de texto sin formato 
-                };
+            };
 
-                transporter.sendMail(mailOptions, function(err, info) {
-
-
-                    console.log(info);
-                    res.json({ exito: true });
+            transporter.sendMail(mailOptions, function(err, info) {
 
 
-                });
+                console.log(info);
+                res.json({ exito: true });
 
 
-            })
-        })
+            });
+
+
+        });
+
 
     } catch (error) {
         res.state(404).json({ error });
@@ -141,57 +132,51 @@ subirarchivosCtrl.updateFixes = (req, res) => {
 }
 
 subirarchivosCtrl.getProyectos = (req, res) => {
-    ibmdb.open(connStr, (err, conn) => {
 
-        conn.query("SELECT * FROM proyecto", (err, data) => {
-            if (err) {
-                res.json({ error: err })
-                console.log("Hubo un error en la busqueda de proyectos" + JSON.stringify(err));
-            } else {
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos");
-                })
-                res.json(data);
-            }
-        })
-    })
+
+    db.query("SELECT * FROM proyecto", (err, data) => {
+        if (err) {
+            res.json({ error: err })
+            console.log("Hubo un error en la busqueda de proyectos" + JSON.stringify(err));
+        } else {
+
+            res.json(data);
+        }
+    });
+
 }
 
 subirarchivosCtrl.getProyectosById = (req, res) => {
     let id_usuario = req.params.id_usuario;
 
-    ibmdb.open(connStr, (err, conn) => {
 
-        conn.query(`SELECT * FROM proyecto WHERE usuario_id_usuario= '${id_usuario}'`, (err, data) => {
-            if (err) {
-                res.json({ error: err })
-                console.log("Hubo un error en la busqueda de proyectos" + JSON.stringify(err));
-            } else {
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos");
-                })
-                res.json(data);
-            }
-        })
-    })
+
+    db.query(`SELECT * FROM proyecto WHERE usuario_id_usuario= '${id_usuario}'`, (err, data) => {
+        if (err) {
+            res.json({ error: err })
+            console.log("Hubo un error en la busqueda de proyectos" + JSON.stringify(err));
+        } else {
+
+            res.json(data);
+        }
+    });
+
 }
 subirarchivosCtrl.getProyectosByNombre = (req, res) => {
     let nombre_proyecto = req.params.nombre_proyecto;
 
-    ibmdb.open(connStr, (err, conn) => {
 
-        conn.query(`SELECT * FROM proyecto WHERE nombre_proyecto= '${nombre_proyecto}'`, (err, data) => {
-            if (err) {
-                res.json({ error: err })
-                console.log("Hubo un error en la busqueda de proyectos" + JSON.stringify(err));
-            } else {
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos");
-                })
-                res.json(data);
-            }
-        })
-    })
+
+    db.query(`SELECT * FROM proyecto WHERE nombre_proyecto= '${nombre_proyecto}'`, (err, data) => {
+        if (err) {
+            res.json({ error: err })
+            console.log("Hubo un error en la busqueda de proyectos" + JSON.stringify(err));
+        } else {
+
+            res.json(data);
+        }
+    });
+
 }
 
 subirarchivosCtrl.getProyectosFacultad = (req, res) => {
@@ -199,20 +184,18 @@ subirarchivosCtrl.getProyectosFacultad = (req, res) => {
     console.log(req.info);
     let id_facultad = req.params.facultad;
 
-    ibmdb.open(connStr, (err, conn) => {
 
-        conn.query(`SELECT * FROM proyecto AS p INNER JOIN asesor AS a ON p.usuario_id_usuario = a.usuario_id_usuario WHERE a.facultad_id_facultad = '${id_facultad}'`, (err, data) => {
-            if (err) {
-                res.json({ error: err })
-                console.log("Hubo un error en la busqueda" + err);
-            } else {
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos")
-                })
-                res.json(data);
-            }
-        })
+
+    db.query(`SELECT * FROM proyecto AS p INNER JOIN asesor AS a ON p.usuario_id_usuario = a.usuario_id_usuario WHERE a.facultad_id_facultad = '${id_facultad}'`, (err, data) => {
+        if (err) {
+            res.json({ error: err })
+            console.log("Hubo un error en la busqueda" + err);
+        } else {
+
+            res.json(data);
+        }
     })
+
 }
 
 subirarchivosCtrl.getProyectosDocente = (req, res) => {
@@ -220,62 +203,55 @@ subirarchivosCtrl.getProyectosDocente = (req, res) => {
     console.log("###", req.params.docente);
     let id_usuario = req.params.docente;
 
-    ibmdb.open(connStr, (err, conn) => {
 
-        conn.query(`SELECT * FROM proyecto AS p INNER JOIN asesor AS a ON p.usuario_id_usuario = a.usuario_id_usuario WHERE a.usuario_id_usuario = '${id_usuario}'`, (err, data) => {
-            if (err) {
-                res.json({ error: err })
-                console.log("Hubo un error en la busqueda" + err);
-            } else {
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos")
-                })
-                res.json(data);
-            }
-        })
-    })
+
+    db.query(`SELECT * FROM proyecto AS p INNER JOIN asesor AS a ON p.usuario_id_usuario = a.usuario_id_usuario WHERE a.usuario_id_usuario = '${id_usuario}'`, (err, data) => {
+        if (err) {
+            res.json({ error: err })
+            console.log("Hubo un error en la busqueda" + err);
+        } else {
+            res.json(data);
+        }
+    });
+
 }
 
 subirarchivosCtrl.getDocumentosEtapa = (req, res) => {
 
-    console.log(req.info);
+
     let etapa = req.params.etapa;
 
-    ibmdb.open(connStr, (err, conn) => {
 
-        conn.query(`SELECT * FROM documento WHERE etapa <= '${etapa}'`, (err, data) => {
-            if (err) {
-                res.json({ error: err })
-                console.log("Hubo un error en la busqueda" + err);
-            } else {
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos")
-                })
-                res.json(data);
-            }
-        })
-    })
+
+    conn.query(`SELECT * FROM documento WHERE etapa <= '${etapa}'`, (err, data) => {
+        if (err) {
+            res.json({ error: err })
+            console.log("Hubo un error en la busqueda" + err);
+        } else {
+
+            res.json(data);
+        }
+    });
+
 }
 
 subirarchivosCtrl.getDocumentosByEtapa = (req, res) => {
 
-    console.log(req.info);
+
     let etapa = req.params.etapa;
 
-    ibmdb.open(connStr, (err, conn) => {
 
-        conn.query(`SELECT * FROM documento WHERE etapa = '${etapa}'`, (err, data) => {
-            if (err) {
-                res.json({ error: err })
-                console.log("Hubo un error en la busqueda" + err);
-            } else {
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos")
-                })
-                res.json(data);
-            }
-        })
-    })
+
+    db.query(`SELECT * FROM documento WHERE etapa = '${etapa}'`, (err, data) => {
+        if (err) {
+            res.json({ error: err })
+            console.log("Hubo un error en la busqueda" + err);
+        } else {
+
+            res.json(data);
+        }
+    });
+
 }
 
 
@@ -290,21 +266,19 @@ subirarchivosCtrl.crearProyecto = (req, res) => {
     var query = `INSERT INTO proyecto (nombre_proyecto, etapa, usuario_id_usuario, fecha , ID_CONVOCATORIA_ID)
     VALUES  ('${nombre}', '${etapa}', '${id_usuario}', CURRENT_DATE , '${id_convocatoria}')`;
 
-    ibmdb.open(connStr, (err, conn) => {
 
-        conn.query(query, (err, data) => {
 
-            if (err) {
-                res.json({ error: err })
-                console.log("Hubo un error en la creacion del proyecto" + err);
-            } else {
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos ")
-                })
-                res.json("Se creo el proyecto")
-            }
-        });
-    })
+    db.query(query, (err, data) => {
+
+        if (err) {
+            res.json({ error: err })
+            console.log("Hubo un error en la creacion del proyecto" + err);
+        } else {
+
+            res.json("Se creo el proyecto")
+        }
+    });
+
 }
 
 subirarchivosCtrl.proyectoDuplicado = (req, res) => {
@@ -313,18 +287,16 @@ subirarchivosCtrl.proyectoDuplicado = (req, res) => {
 
     var query = `SELECT COUNT(*) AS duplicate from proyecto where nombre_proyecto='${nombre}'`;
 
-    ibmdb.open(connStr, function(err, conn) {
-        if (err) return console.log(err);
 
-        conn.query(query, function(err, data) {
+    if (err) return console.log(err);
 
-            if (err) res.json({ error: err })
-            else res.json(data)
-            conn.close(function() {
-                console.log('termino de buscar');
-            });
-        });
+    db.query(query, function(err, data) {
+
+        if (err) res.json({ error: err })
+        else res.json(data)
+
     });
+
 }
 
 subirarchivosCtrl.updateProject = (req, res) => {
@@ -334,20 +306,17 @@ subirarchivosCtrl.updateProject = (req, res) => {
     const corregido = req.body.corregido;
     console.log(`llego esto correcciones:${correcciones} y corregido : ${corregido}`);
     try {
-        ibmdb.open(connStr, (err, conn) => {
 
-            conn.query(`UPDATE proyecto SET correcciones = '${correcciones}', corregido = '${corregido}' WHERE id_proyecto = '${ID_PROYECTO}'`, (err, data) => {
-                if (err) {
-                    res.json({ error: err })
-                    console.log("Hubo un error en la busqueda" + err);
-                } else {
-                    conn.close(() => {
-                        console.log("Se ha cerrado la base de datos")
-                    })
-                    res.json(data);
-                }
-            })
-        })
+
+        db.query(`UPDATE proyecto SET correcciones = '${correcciones}', corregido = '${corregido}' WHERE id_proyecto = '${ID_PROYECTO}'`, (err, data) => {
+            if (err) {
+                console.log("Hubo un error en la busqueda" + err);
+            } else {
+
+                res.json(data);
+            }
+        });
+
     } catch (error) {
         res.state(404).json({ error });
     }
@@ -361,38 +330,36 @@ subirarchivosCtrl.updateStageProjects = (req, res) => {
         const etapa = req.body.etapa;
         const correo = req.body.correo;
         const nombreProyecto = req.body.nombreProyecto;
-        ibmdb.open(connStr, (err, conn) => {
-
-            conn.query(`UPDATE PROYECTO SET ETAPA = '${etapa}',CORREGIDO = '${false}',CORRECCIONES = '${false}' WHERE ID_PROYECTO = '${ID_PROYECTO}' `, (err, data) => {
-
-                conn.close(() => {
-                    console.log("Se ha cerrado la base de datos")
-                })
 
 
-                const mailOptions = {
-                    from: 'consultorio.usta.DRSU@gmail.com', // dirección del remitente 
-                    to: `${correo}`, // lista de los destinatarios del 
-                    subject: `el proyecto ${nombreProyecto} ha pasado a la siguiente etapa`, // Línea del asunto 
-                    html: `<h1>CORRECCIONES PENDIENTES</h1>
+        db.query(`UPDATE PROYECTO SET ETAPA = '${etapa}',CORREGIDO = '${false}',CORRECCIONES = '${false}' WHERE ID_PROYECTO = '${ID_PROYECTO}' `, (err, data) => {
+
+
+
+
+            const mailOptions = {
+                from: 'consultorio.usta.DRSU@gmail.com', // dirección del remitente 
+                to: `${correo}`, // lista de los destinatarios del 
+                subject: `el proyecto ${nombreProyecto} ha pasado a la siguiente etapa`, // Línea del asunto 
+                html: `<h1>CORRECCIONES PENDIENTES</h1>
                             <p>tu proyecto <b>${nombreProyecto}</b> ha cumplido con los requisitos de la etapa actual y ha avanzado a la siguiente etapa</p>
                             <p>puedes entrar a la plataforma para seguir con el proceso del proyecto</p> ` // cuerpo de texto sin formato 
-                };
+            };
 
-                transporter.sendMail(mailOptions, function(err, info) {
-
-
-                    console.log(info);
-                    res.json({ exito: true });
+            transporter.sendMail(mailOptions, function(err, info) {
 
 
-                });
+                console.log(info);
+                res.json({ exito: true });
 
 
-                res.json(data);
+            });
 
-            })
-        })
+
+            res.json(data);
+
+        });
+
 
     } catch (error) {
         res.state(404).json({ error });
---
+++ Fuentes/Back/db_connection.js
@@ -0,0 +1,12 @@
+var ibmdb = require("ibm_db"),
+    connString = "DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=vnh98091;PWD=4f-t4n3qkgv6vpb4;"; //THOMAS;
+
+try {
+    var option = { connectTimeout: 60, systemNaming: true }; // Connection Timeout after 40 seconds.
+    var conn = ibmdb.openSync(connString, option);
+
+} catch (e) {
+    console.log(e.message);
+}
+
+module.exports = conn;
